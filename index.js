19e371b1868b7f06b314b55e2c4ffc6c8fb1530e
duan spot <brandennjonge@gmail.com>
2023-06-29 11:59:31 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -17,10 +17,16 @@ isPalindrome("racecar");
 
 /* 
   Add your pseudocode here
+  get the words into a varible
+  split the words
+  reverse the words 
+  join the words
+
 */
 
 /*
   Add written explanation of your solution here
+  solution reverses the words and then checks to see if they match up
 */
 
 // You can run `node index.js` to view these console logs
